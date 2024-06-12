350177e5652d5cc66e03d00d102befb1f2678cc8
Grzegorz Prabucki <132297553+Grzegorztestowy@users.noreply.github.com>
2024-06-12 21:53:53 +0200

    project modification, adding functionality, removing unnecessary files

---
+++ src/Form/index.js
@@ -0,0 +1,92 @@
+import "./style.css";
+import { useState } from "react";
+import currencies from "../Curencies";
+import Result from "../Result";
+
+const Form = () => {
+  const [amount, setAmount] = useState("");
+  const [result, setResult] = useState(null);
+  const [selectedCurrencyValue, setSelectedCurrencyValue] = useState({
+    worth: currencies[0].worth,
+    shrt: currencies[0].shrt,
+  });
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    const foundCurrency = currencies.find(it => it.worth === parseFloat(selectedCurrencyValue.worth));
+    if (foundCurrency) {
+      setResult({
+        targetAmount: +amount,
+        myResult: amount / foundCurrency.worth,
+        selectedCurrency: foundCurrency.shrt,
+      });
+      setAmount("");
+    }
+  };
+
+  const selectOption = currencies.map((currency) => (
+    <option key={currency.id} value={currency.worth}>
+      {currency.shrt}
+    </option>
+  ));
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Kalkulator Walut</legend>
+        <p className="form__paragraph">
+          <label>
+            <span className="form__labelText">Kwota w zł*:</span>
+            <input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              className="form__field"
+              type="number"
+              name="value"
+              required
+              placeholder="wpisz/ustaw kwotę"
+              step="0.01"
+              min={"0"}
+            />
+          </label>
+        </p>
+        <p className="form__paragraph">
+          <label>
+            <span className="form__labelText">Waluta:</span>
+            <select
+              className="form__field"
+              value={selectedCurrencyValue.worth}
+              onChange={({ target }) => {
+                const selectedCurrency = currencies.find(currency => currency.worth === parseFloat(target.value));
+                setSelectedCurrencyValue({
+                  worth: selectedCurrency.worth,
+                  shrt: selectedCurrency.shrt,
+                });
+              }}
+            >
+              {selectOption}
+            </select>
+          </label>
+        </p>
+      </fieldset>
+      <p>
+        <button type="submit" className="form__button">
+          Przelicz
+        </button>
+      </p>
+      <p className="paragraph">
+      {result && (
+        <strong>
+          <Result
+            targetAmount={result.targetAmount}
+            myResult={result.myResult}
+            selectedCurrency={result.selectedCurrency}
+          />
+          </strong>
+      )}
+      </p>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/Result/index.js
@@ -0,0 +1,7 @@
+const Result = ({targetAmount, myResult, selectedCurrency}) => {
+    return (
+      `${targetAmount} PLN = ${myResult.toFixed(2)} ${selectedCurrency}`
+      );
+  };
+  
+  export default Result;
